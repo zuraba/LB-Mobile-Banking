6f96f5d8d4244dcf07c2a77fe1eb30f02057f2d7
zura <zkumsiashvili@gmail.com>
2013-03-04 18:42:27 +0400

    layouts added

--- gen/ge/android/liberty/lbmobinebanking/R.java
+++ gen/ge/android/liberty/lbmobinebanking/R.java
@@ -44,25 +44,26 @@ public final class R {
     }
     public static final class id {
         public static final int RelativeLayout1=0x7f090000;
-        public static final int action_settings=0x7f090013;
-        public static final int button1=0x7f090005;
-        public static final int button2=0x7f090008;
-        public static final int button3=0x7f090011;
-        public static final int button4=0x7f090012;
-        public static final int button5=0x7f090004;
-        public static final int button6=0x7f090003;
-        public static final int button7=0x7f090002;
-        public static final int button8=0x7f09000d;
-        public static final int button9=0x7f090006;
-        public static final int imageView1=0x7f09000c;
-        public static final int relativeLayout2=0x7f090010;
+        public static final int action_settings=0x7f090014;
+        public static final int button1=0x7f090011;
+        public static final int button2=0x7f090012;
+        public static final int button3=0x7f090003;
+        public static final int button4=0x7f090004;
+        public static final int button5=0x7f090005;
+        public static final int button6=0x7f090006;
+        public static final int button7=0x7f090007;
+        public static final int button8=0x7f090008;
+        public static final int button9=0x7f090009;
+        public static final int imageView1=0x7f090010;
+        public static final int linearLayout1=0x7f09000f;
+        public static final int relativeLayout2=0x7f090002;
         public static final int textView1=0x7f090001;
-        public static final int textView2=0x7f090007;
-        public static final int textView3=0x7f09000a;
-        public static final int textView4=0x7f090009;
+        public static final int textView2=0x7f090013;
+        public static final int textView3=0x7f09000b;
+        public static final int textView4=0x7f09000a;
         public static final int textView5=0x7f09000e;
-        public static final int textView6=0x7f09000f;
-        public static final int textView7=0x7f09000b;
+        public static final int textView6=0x7f09000c;
+        public static final int textView7=0x7f09000d;
     }
     public static final class layout {
         public static final int activity_first_screen=0x7f030000;
@@ -83,6 +84,7 @@ public final class R {
         public static final int login=0x7f060003;
         public static final int messages=0x7f060009;
         public static final int sign_up=0x7f060004;
+        public static final int us_flag=0x7f06000b;
     }
     public static final class style {
         /** 
