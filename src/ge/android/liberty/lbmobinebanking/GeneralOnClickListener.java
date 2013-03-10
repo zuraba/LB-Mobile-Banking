14f0b2b0057fd55aef1327a32f1cf9896c974247
zura <zkumsiashvili@gmail.com>
2013-03-10 11:41:54 +0400

    new drawables

---
+++ src/ge/android/liberty/lbmobinebanking/GeneralOnClickListener.java
@@ -0,0 +1,35 @@
+package ge.android.liberty.lbmobinebanking;
+
+import ge.android.liberty.lbmobilebanking.login.LoginMain;
+import android.app.Activity;
+import android.content.Context;
+import android.content.Intent;
+import android.view.View;
+import android.view.View.OnClickListener;
+
+public class GeneralOnClickListener implements OnClickListener {
+
+	private Context context;
+	Intent intent;
+
+	public GeneralOnClickListener(Context context) {
+		this.context = context;
+	}
+
+	@Override
+	public void onClick(View v) {
+		if (v.getId() == R.id.button5) {
+			((Activity) context).finish();
+			return;
+		}
+		if (v.getId() == R.id.button1) {
+			intent = new Intent(context, LoginMain.class);
+			context.startActivity(intent);
+			return;
+		}
+		if (v.getId() == R.id.btn_back) {
+			((Activity) context).finish();
+			return;
+		}
+	}
+}
